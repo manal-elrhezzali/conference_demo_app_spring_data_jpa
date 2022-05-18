08b83b2bb8e6202338b7d158746088a16302b84e
El-rhezzali Manal <manal.gzl99@gmail.com>
2022-05-18 22:48:03 +0200

    Paging and sorting

--- src/main/java/com/pluralsight/conferencedemo/repositories/SessionJpaRepository.java
+++ src/main/java/com/pluralsight/conferencedemo/repositories/SessionJpaRepository.java
@@ -2,7 +2,11 @@ package com.pluralsight.conferencedemo.repositories;
 
 import com.pluralsight.conferencedemo.models.Session;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SessionJpaRepository extends JpaRepository<Session, Long> {
   List<Session> findBySessionNameContains(String name);
@@ -10,5 +14,8 @@ public interface SessionJpaRepository extends JpaRepository<Session, Long> {
   List<Session> findBySessionNameNotLike(String name);
   List<Session> findBySessionNameStartingWith(String name);
   List<Session> findBySessionLengthLessThan(Integer sessionLength);
+
+  @Query("select s from Session s where s.sessionName like %:name")
+  Page<Session> getSessionsWithName(@Param("name") String name, Pageable pageable);
 
 }
--- src/test/java/com/pluralsight/conferencedemo/models/SessionTest.java
+++ src/test/java/com/pluralsight/conferencedemo/models/SessionTest.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -48,5 +52,12 @@ public class SessionTest {
     public void testJpaLessThan() throws Exception {
         List<Session> sessions = jpaRepository.findBySessionLengthLessThan(45);
         assertTrue(sessions.size() > 0);
+    }
+
+    @Test
+    public void testPagingSorting() throws Exception {
+        Page<Session> page = jpaRepository.getSessionsWithName("S", PageRequest.of(1,5, Sort.by(
+            Direction.DESC, "sessionLength")));
+        assertTrue(page.getTotalElements() > 0);
     }
 }
